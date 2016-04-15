e7dddee54a608530a8805e2dec756f1418288b78
StewartTaylor <stewart.taylor@oit.edu>
2016-04-15 12:02:17 -0700

    Philosoraptor quotes updated.

--- Puzzles/Week 3/src/magic_8_ball.cpp
+++ Puzzles/Week 3/src/magic_8_ball.cpp
@@ -11,16 +11,16 @@ magic_8_ball::magic_8_ball()
     m_sayings.append("Zac Efron.");
     m_sayings.append("Ronda Rousey.");
 
-    m_questions.append("If violent games make people violent, then does Farmville make people farmers?");
+    m_questions.append("If violent video games make people violent, then does Farmville make people farmers?");
     m_questions.append("If the school says sleep is important, ever wonder why does lab start at 8 am?");
     m_questions.append("If I download a movie in Jamaica, am I a pirate of the carribbean?");
-    m_questions.append("While you're waiting for the waiter, in that moment do you not become the waiter?");
-    m_questions.append("If a vampire bites a zombie, will it turn into a zombie or will the zombie turn into a vampire?");
+    m_questions.append("If you're too hott for me, doesn't that make me cooler than you?");
+    m_questions.append("If practice makes perfect and no one's perfect, why practice?");
     m_questions.append("In an abacus factory, being counter productive would be a good thing, right?");
     m_questions.append("If being hipster is mainstream and I am not a hipster Does that make me a hipster?");
     m_questions.append("if you put lil wayne and lil john together would you have a full size john wayne?");
-    m_questions.append("those first two guys who thought superman was a bird or a plane ever wonder why were they so excited?");
-    m_questions.append("If a word in the dictionary is misspelled ever wonder How we Know?");
+    m_questions.append("If time is money, is an atm a time machine?");
+    m_questions.append("Can a teacher give a homeless kid, homework?");
     m_questions.append("if the earth is the third planet from the sun then isn't every country a third world country?");
     m_questions.append("If the zombie apocalypse happens in Vegas, would it stay in Vegas?");
     m_questions.append("if a girl from iceland and a guy from cuba have a kid... will he be an icecube?");
