0767bc972f363639fecbc60ed8566ef55ef9c40f
StewartTaylor <stewart.taylor@oit.edu>
2016-04-16 16:47:01 -0700

    Starting traffic light puzzle.

---
+++ Puzzles/Week 3/extra/extra_credit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func()
+{
+	// your code here
+	// you can only add code in the body of main
+}
+
+// You are NOT allowed to change anything in main
+int main(int argc, char * argv[])
+{
+	int i = 2;
+	func();
+        i = 500;
+	printf(%d, i);
+	return 0;
+}
